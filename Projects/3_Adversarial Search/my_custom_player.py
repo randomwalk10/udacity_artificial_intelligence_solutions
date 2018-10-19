e0a8d814688a85ee7ca1d80f554356895225c396
Xuemeng Zhang <nxzhangxm@gmail.com>
2018-10-19 14:12:58 -0500

    oct 19, 2018: -modified my_customer_player.py; -able to complete games; -need to solve problems related with data.pickle

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,5 +1,8 @@
 
 from sample_players import DataPlayer
+from collections import defaultdict, Counter
+import pickle
+import random
 
 
 class CustomPlayer(DataPlayer):
@@ -28,12 +31,12 @@ class CustomPlayer(DataPlayer):
 
         This method must call self.queue.put(ACTION) at least once, and may
         call it as many times as you want; the caller is responsible for
-        cutting off the function after the search time limit has expired. 
+        cutting off the function after the search time limit has expired.
 
         See RandomPlayer and GreedyPlayer in sample_players for more examples.
 
         **********************************************************************
-        NOTE: 
+        NOTE:
         - The caller is responsible for cutting off search, so calling
           get_action() from your own code will create an infinite loop!
           Refer to (and use!) the Isolation.play() function to run games.
@@ -43,7 +46,64 @@ class CustomPlayer(DataPlayer):
         #       method by combining techniques from lecture
         #
         # EXAMPLE: choose a random move without any search--this function MUST
-        #          call self.queue.put(ACTION) at least once before time expires
-        #          (the timer is automatically managed for you)
-        import random
-        self.queue.put(random.choice(state.actions()))
+        #          call self.queue.put(ACTION) at least once before time
+        #          expires (the timer is automatically managed for you)
+        # import random
+        # self.queue.put(random.choice(state.actions()))
+
+        # initialize and load data.pickle
+        NUM_ROUNDS = 10
+        if not self.data:
+            self.data = defaultdict(Counter)
+
+        # run openbook of depth level = 4
+        for _ in range(NUM_ROUNDS):
+            self.build_tree(state)
+
+        # return an action of current state with the highest reward
+        self.queue.put(max(self.data[state], key=self.data[state].get))
+
+    def __del__(self):
+        if self.data:
+            filename = "data.pickle"
+            f = open(filename, 'wb')
+            pickle.dump(self.data, f)
+            f.close()
+
+    def build_tree(self, state, depth=4):
+        """randomly choose an action from current state, until the game
+        terminates or reaches the maximum depth level, and update this action
+        at this state with the estimated reward, calculated by function
+        "simulate"
+
+        :state: game state
+        :book: dictionary that maps state and action to reward(integer)
+        :depth: search depth level(>=0)
+        :returns: reward to the upper level(the other player)
+
+        """
+        # check if depth level reaches zero or game terminates
+        if depth <= 0 or state.terminal_test():
+            return -self.simulate(state)
+        # randomly choose an action from current state
+        action = random.choice(state.actions())
+        # search into subtrees and get the reward for state and action
+        reward = self.build_tree(state.result(action), depth-1)
+        self.data[state][action] += reward
+        # return -reward for upper level(the other player)
+        return -reward
+
+    def simulate(self, state):
+        """randomly search down through game tree from current state,
+        until the game terminates, return +1 if the active player of
+        current state wins, -1 otherwise
+
+        :state: game state
+        :returns: reward(+1 if the active player of current state wins,
+        -1 otherwise
+
+        """
+        player_id = state.player()
+        while not state.terminal_test():
+            state = state.result(random.choice(state.actions()))
+        return -1 if state.utility(player_id) < 0 else 1
